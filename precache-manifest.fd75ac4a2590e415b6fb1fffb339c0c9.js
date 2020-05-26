ba54791bfa2e3bff1f92fccf69abf4fe71544efa
Richard Westenra <rjwestenra@gmail.com>
2020-05-26 17:19:01 +0100

    Demo build [ci skip]

---
+++ precache-manifest.fd75ac4a2590e415b6fb1fffb339c0c9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08922128b54ecf68203850e7b9d8a40d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aa3adcc4a50f50f27619",
+    "url": "./static/css/2.fcf86715.chunk.css"
+  },
+  {
+    "revision": "16601d318c15c73e5967",
+    "url": "./static/css/main.b4a43fc3.chunk.css"
+  },
+  {
+    "revision": "aa3adcc4a50f50f27619",
+    "url": "./static/js/2.a1c1dd70.chunk.js"
+  },
+  {
+    "revision": "54120cd43cbbd1b3b4c14b86ca02f064",
+    "url": "./static/js/2.a1c1dd70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16601d318c15c73e5967",
+    "url": "./static/js/main.f626a013.chunk.js"
+  },
+  {
+    "revision": "c88a607ed4c298f69081",
+    "url": "./static/js/runtime-main.6cb937c6.js"
+  },
+  {
+    "revision": "6259cc84bb236512f69bc1d20397254c",
+    "url": "./static/media/brodyquest.6259cc84.mp3"
+  },
+  {
+    "revision": "e702bd5fc3663eda7c4459232f5a76c0",
+    "url": "./static/media/easter-egg.e702bd5f.png"
+  }
+]);
